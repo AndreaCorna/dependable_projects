6301de2e055bdf2aa20ce18be49c346e4c549ed3
Andrea Corna <andrea.corna.ac.91@gmail.com>
2015-06-17 21:58:28 +0200

    update setup
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- setup.sh
+++ setup.sh
@@ -10,17 +10,21 @@ cd caliper
 make
 cp caliper ../script
 cp computeAlpha ../script
+chmod +x ../script/caliper
+chmod +x ../script/computeAlpha
 cd ..
 
 echo "Compiling Hotspot..."
 cd hotspot
 make
 cp hotspot ../script
+chmod +x ../script/hotspot
 cd ..
 
 echo "Compiling Hotspot_Gui..."
 cd Hotspot_Gui
 qmake Hotspot_Gui.pro
 make
+chmod +x Hotspot_Gui
 
 echo "Finished compilation"
